617737f93b0fbdbdc806ed6761c01f983d4e9ef0
JK.Min <75975464+calendar3450@users.noreply.github.com>
2026-04-22 14:02:21 +0900

    [level 2] Title: 리코쳇 로봇, Time: 6.78 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.py
@@ -4,70 +4,37 @@ def solution(board):
     row = len(board)
     col = len(board[0])
     
-    
     for i in range(row):
         for j in range(col):
-            if board[i][j] == 'G':
-                gy, gx = i,j
             if board[i][j] == 'R':
-                ry , rx = i,j
-    
+                rx = j
+                ry =i
+            if board[i][j] == 'G':
+                gx = j
+                gy = i
+                
     if 0 <gx< col-1 and 0 <gy < row-1 and board[gy][gx+1] == '.' and board[gy][gx-1] == '.' and board[gy+1][gx] == '.' and board[gy-1][gx] == '.':
         return -1
     
-    visited = [[False] * col for _ in range(row)]
+    visited = [[False]*col for _ in range(row)]
     visited[ry][rx] = True
-    q = deque([(ry, rx, 0)])
+    queue = deque([(rx,ry,0)])
     
-    while q:
-        y, x, cnt = q.popleft()
+    while queue:
+        x,y,cnt = queue.popleft()
         
         if board[y][x] == 'G':
             return cnt
         
-        for dy,dx in [(0,1),(0,-1),(1,0),(-1,0)]:
+        for dx,dy in [(1,0),(-1,0),(0,1),(0,-1)]:
             ny,nx = y,x
             
             while 0<= ny+dy < row and 0<= nx+dx < col and board[ny+dy][nx+dx] != 'D':
                 ny += dy
                 nx += dx
                 
-            # 멈춘 위치가 미방문이면 큐에 추가
             if not visited[ny][nx]:
                 visited[ny][nx] = True
-                q.append((ny, nx, cnt+1))
-                
+                queue.append((nx,ny,cnt+1))
+        
     return -1
-
-
-# from collections import deque
-# def solution(board):
-#     row = len(board)
-#     col = len(board[0])
-    
-#     for i in range(row):
-#         for j in range(col):
-#             if board[i][j] == 'R':
-#                 sy, sx = i, j
-    
-#     visited = [[False] * col for _ in range(row)]
-#     visited[sy][sx] = True
-#     q = deque([(sy, sx, 0)])
-    
-#     while q:
-#         y, x, cnt = q.popleft()
-        
-#         if board[y][x] == 'G':
-#             return cnt
-        
-#         for dy, dx in [(0,1),(0,-1),(1,0),(-1,0)]:
-#             ny, nx = y, x
-#             while 0<=ny+dy<row and 0<=nx+dx<col and board[ny+dy][nx+dx] != 'D':
-#                 ny += dy
-#                 nx += dx
-            
-#             if not visited[ny][nx]:
-#                 visited[ny][nx] = True
-#                 q.append((ny, nx, cnt+1))
-    
-#     return -1
