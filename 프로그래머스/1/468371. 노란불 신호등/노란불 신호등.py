c3a85990cdcc52596a6ef0b594ce84f5e73b95e9
JK.Min <75975464+calendar3450@users.noreply.github.com>
2026-04-27 16:25:51 +0900

    [level 1] Title: 노란불 신호등, Time: 2044.88 ms, Memory: 32 MB -BaekjoonHub

--- 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.py
+++ 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.py
@@ -2,7 +2,7 @@ def solution(signals):
     answer = 0
     n = len(signals)
     ans = [0] * 3000000
-    
+    # 300백만초 안에는 답은 있겠지~
     for i in signals:
         for j in range(3000000//sum(i)):
             for k in range(i[1]):
