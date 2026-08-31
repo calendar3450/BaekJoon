35e1cbcdddb2123e87e283dc57977ce85c931fb3
JK.Min <75975464+calendar3450@users.noreply.github.com>
2026-09-01 00:08:09 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.03 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -1,5 +1,12 @@
 def solution(s):
-    d=list(map(int,str(s).split()))
-    d.sort()
-    answer=str(d[0])+str(' ')+str(d[-1])
+    answer = ''
+    s_list = list(s.split(' '))
+    s_list2 =[]
+    
+    for s_l in s_list:
+        s_list2.append(int(s_l))
+        
+    s_list2.sort()
+    
+    answer = str(s_list2[0])+" "+str(s_list2[-1])
     return answer
