259e95c69a53cc2ec7e81c8ee51dcff8dd4be4ca
JK.Min <75975464+calendar3450@users.noreply.github.com>
2026-09-04 10:59:09 +0900

    [level 2] Title: 숫자 변환하기, Time: 166.54 ms, Memory: 19 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
@@ -1,29 +1,16 @@
 def solution(x, y, n):
-    if x== y:
-        return 0
-    
     answer = 0
-    dp=[99999999] * (y+1)
+    dp= [9999999] * (y+1)
     dp[x] = 0
     
-    for i in range(x+1,y+1):
-        if i//2<x and i//3<x and i-n<x:
-            continue
+    for i in range(x,y):
+        if dp[i] <9999999:
+            if i *2 <=y:
+                dp[i*2] = min(dp[i] +1 , dp[i*2])
+            if i*3 <=y:
+                dp[i*3] = min(dp[i]+1,dp[i*3])
+            if i+n <=y:
+                dp[i+n] = min(dp[i]+1,dp[i+n])
             
-        if i%6==0:
-            dp[i] = min(dp[i//2]+1,dp[i//3]+1,dp[i-n]+1)
-            
-        elif i%2==0:
-            dp[i] = min(dp[i//2]+1,dp[i-n] +1)
-            
-        elif i%3 ==0:
-            dp[i] = min(dp[i//3] +1, dp[i-n]+1)
-            
-        else:
-            dp[i] =dp[i-n]+1
         
-    
-    if dp[y] >= 99999999:
-        return -1
-    
-    return dp[y]
+    return -1 if dp[y] == 9999999 else dp[y]
