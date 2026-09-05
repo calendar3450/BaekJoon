510973744a2cbca135be59197af16f5bef07cfd9
JK.Min <75975464+calendar3450@users.noreply.github.com>
2026-09-05 17:05:08 +0900

    [level 2] Title: 구명보트, Time: 7.93 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,15 +1,16 @@
 def solution(people, limit):
     answer = 0
-    l, r = 0,len(people) -1
     people.sort()
-    print(people)
+    n = len(people)
+    arrival = 0
+    l = 0
+    r = n-1
+    
     while l<=r:
-        if people[l] + people[r] <=limit:
-            answer +=1
+        if people[l] +people[r] <= limit:
             l+=1
-            r-=1
-        elif people[l] + people[r] >limit:
-            r-=1
-            answer +=1
             
+        r-=1
+        answer +=1
+        
     return answer
